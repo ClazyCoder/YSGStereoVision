9a5412442426cf948e32bc044a18143492785e98
CLazyCoder <xots899@naver.com>
2022-01-06 19:28:49 +0900

    Calibrator 클래스 추가

--- StereoModule/Calibration.py
+++ StereoModule/Calibration.py
@@ -1,11 +1,22 @@
 import cv2 as cv
 import numpy as np
 import json
+import glob
 
-def LoadDatas(path='./datas'):
-    # TODO: 데이터 불러오기
-    pass
+class Calibrator:
+    def __init__(self, imgWidth, imgHeight):
+        self.imgWidth = imgWidth
+        self.imgHeight = imgHeight
+        self.leftDatas = []
+        self.rightDats = []
+        pass
 
-def RunCalibration():
-    # TODO: 캘리브레이션 수행
-    pass
+    def LoadDatas(self, path='./datas'):
+        leftdata_glob = glob.glob(path+'/data_left*.json')
+        rightdata_glob = glob.glob(path+'/data_left*.json')
+        # TODO: 데이터 불러오기
+        pass
+
+    def RunCalibration(self):
+        # TODO: 캘리브레이션 수행
+        pass
