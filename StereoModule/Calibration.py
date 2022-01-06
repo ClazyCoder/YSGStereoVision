1ceb786bb07c85e94d7bc2cff811539c7b0ebb4c
Yimsungil <xots899@naver.com>
2022-01-06 17:48:46 +0900

    약간의 변경
    코드 일부 수정 및 Calibration.py의 loadData메서드 추가

--- StereoModule/Calibration.py
+++ StereoModule/Calibration.py
@@ -2,6 +2,10 @@ import cv2 as cv
 import numpy as np
 import json
 
-def runCalibration():
+def LoadDatas(path='./datas'):
+    # TODO: 데이터 불러오기
+    pass
+
+def RunCalibration():
     # TODO: 캘리브레이션 수행
     pass
--- main.py
+++ main.py
@@ -1,1 +1,4 @@
+import StereoModule.Calibration as SC
+import StereoModule.StereoMatcher as SSM
+
 # TODO : 전체 프로그램 합치기
