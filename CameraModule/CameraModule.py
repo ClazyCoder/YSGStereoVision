db26b89a442ca4ccd18fc3168e2349d9e4f0e708
Yimsungil <xots899@naver.com>
2022-01-11 16:53:02 +0900

    CameraModule.py 내용 추가
    StereoCamera 클래스의 생성자와 GetFrame 메서드의 구현

--- CameraModule/CameraModule.py
+++ CameraModule/CameraModule.py
@@ -1,7 +1,25 @@
 import cv2 as cv
 
 class StereoCamera:
-    def __init__(self):
-        pass
-    def GetFrame():
-        pass
+    def __init__(self,imgSize):
+        self.leftCamera = cv.VideoCapture(0, cv.CAP_DSHOW)
+        self.leftCamera.set(cv.CAP_PROP_AUTOFOCUS, 0)
+        self.leftCamera.set(cv.CAP_PROP_AUTO_EXPOSURE,0)
+        self.leftCamera.set(cv.CAP_PROP_AUTO_WB,0)
+        self.leftCamera.set(cv.CAP_PROP_FRAME_WIDTH, imgSize[0])
+        self.leftCamera.set(cv.CAP_PROP_FRAME_HEIGHT, imgSize[1])
+
+        self.rightCamera = cv.VideoCapture(1, cv.CAP_DSHOW)
+        self.rightCamera.set(cv.CAP_PROP_AUTOFOCUS, 0)
+        self.rightCamera.set(cv.CAP_PROP_AUTO_EXPOSURE,0)
+        self.rightCamera.set(cv.CAP_PROP_AUTO_WB,0)
+        self.rightCamera.set(cv.CAP_PROP_FRAME_WIDTH, imgSize[0])
+        self.rightCamera.set(cv.CAP_PROP_FRAME_HEIGHT, imgSize[1])
+        
+    def GetFrame(self):
+        if not (self.leftCamera.grab()and self.rightCamera.grab()):
+            print("No more frames")
+            return None
+        ret_left, frame_left = self.leftCamera.retrieve()
+        ret_right, frame_right = self.rightCamera.retrieve()
+        return (frame_left, frame_right)
