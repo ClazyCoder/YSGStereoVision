8dbe54216bc94689128970afe90043a157e068e2
CLazyCoder <xots899@naver.com>
2022-01-12 21:08:12 +0900

    Main 기본 구성 생각

--- main.py
+++ main.py
@@ -5,7 +5,14 @@ import StereoModule.StereoMatcher as SSM
 import CameraModule.CameraModule as CM
 
 def main():
-    pass
+    StereoCam = CM.StereoCamera((640,480))
+    while True:
+        left, right = StereoCam.GetFrame()
+        # TODO : Depth값 추정
+        k = cv.waitKey(1)
+        if k == 99:
+            break
+    
 
 if __name__ == "__main__":
     main()
