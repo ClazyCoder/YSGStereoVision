3d513f6fe95cfb3dfe90e578bd7846dfe87c616e
CLazyCoder <xots899@naver.com>
2022-01-17 21:05:17 +0900

    main.py의 실험적 내용 변경 및 Calibration.py의 접선왜곡에 대한 패러미터 변경
    main.py - 매칭 패러미터 변경 및 rectification 과정의 패러미터 수정, 출력 부분에서의 depth 및 필터링 과정을 통한 disparity 추가.
    Calibration.py - 캘리브레이션 과정에서 접선왜곡은 존재하지 않는다고 고정하고 캘리브레이션 진행.

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ def main():
     stereoCam = CM.StereoCamera(IMGSIZE)
     calibrator = SC.Calibrator(IMGSIZE)
     calibrator.LoadCalibrationDatas()
-    stereoMatcher = SSM.StereoMatcher(SSM.STEREO_TYPE_BM,112,15)
+    stereoMatcher = SSM.StereoMatcher(SSM.STEREO_TYPE_BM,96,15)
     # stereoMatcher.leftMatcher.set...
     stereoMatcher.CreateWlsFilter()
 
     K1, K2, D1, D2, R, T = calibrator.K1, calibrator.K2, calibrator.D1, calibrator.D2, calibrator.R, calibrator.T
-    R1, R2, P1, P2, Q, roi_left, roi_right = cv.stereoRectify(K1, D1, K2, D2, IMGSIZE, R, T ,alpha=0)
+    R1, R2, P1, P2, Q, roi_left, roi_right = cv.stereoRectify(K1, D1, K2, D2, IMGSIZE, R, T ,alpha=-1)
     leftMapX, leftMapY = cv.initUndistortRectifyMap(K1, D1, R1, P1, IMGSIZE, cv.CV_32FC1)
     rightMapX, rightMapY = cv.initUndistortRectifyMap(K2, D2, R2, P2, IMGSIZE, cv.CV_32FC1)
 
@@ -29,15 +29,15 @@ def main():
         gray_right = cv.cvtColor(right_rectified, cv.COLOR_BGR2GRAY)
 
         disparity = stereoMatcher.GetDisparity(gray_left, gray_right)
+        filteredDisp = stereoMatcher.GetFilteredDisparity(gray_left,gray_right)
         image3D = cv.reprojectImageTo3D(disparity, Q)
-        depth = image3D[:,:,2]
-
         cv.imshow('left',leftImg)
         cv.imshow('right',rightImg)
         cv.imshow('rectified_left',left_rectified)
         cv.imshow('rectified_right',right_rectified)
-        cv.imshow('disparity',disparity)
-        cv.imshow('depth',depth)
+        cv.imshow('disparity',disparity/ np.max(disparity))
+        cv.imshow('filteredDisparity',filteredDisp/np.max(filteredDisp))
+        cv.imshow('depth',image3D[:,:,2]/np.max(image3D[:,:,2]))
 
         k = cv.waitKey(1)
         if k == 27:
