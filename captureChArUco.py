a45007ccc92d0be621dca8a4c3460fc4bb9b12ce
CLazyCoder <xots899@naver.com>
2022-01-21 22:46:30 +0900

    ChArUco 테스트

--- captureChArUco.py
+++ captureChArUco.py
@@ -1,9 +1,11 @@
 import cv2 as cv
 import numpy as np
-
+from cv2 import aruco
 def main():
-    # TODO : ChArUco 보드 캡쳐하고 포인트 저장.
-    pass
+    aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+    board = aruco.CharucoBoard_create(7, 5, 1, .8, aruco_dict)
+    imboard = board.draw((2000, 2000))
+    cv.imwrite("./chessboard.tiff", imboard)
 
 if __name__ == "__main__":
     main()
