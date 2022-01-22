e8ee12e9e7c4adc8c04edc059ed1f69e0a502ded
CLazyCoder <xots899@naver.com>
2022-01-22 21:20:21 +0900

    captureChArUco.py 내용추가

--- captureChArUco.py
+++ captureChArUco.py
@@ -1,7 +1,14 @@
 import cv2 as cv
 import numpy as np
 from cv2 import aruco
+import CameraModule.CameraModule as CM
+import os
+
 def main():
+    if not os.path.isdir('./Aruco_captures'):
+        os.mkdir('./Aruco_captures')
+    if not os.path.isdir('./Aruco_datas'):
+        os.mkdir('./Aruco_datas')
     aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
     board = aruco.CharucoBoard_create(7, 5, 1, .8, aruco_dict)
     imboard = board.draw((2000, 2000))
