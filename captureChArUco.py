11afc432a3636813336373cbf238da0bdfd71c8d
CLazyCoder <xots899@naver.com>
2022-01-23 22:33:13 +0900

    captureChArUco.py 내용추가.

--- captureChArUco.py
+++ captureChArUco.py
@@ -4,6 +4,8 @@ from cv2 import aruco
 import CameraModule.CameraModule as CM
 import os
 
+IMGSIZE = (640,480)
+
 def main():
     if not os.path.isdir('./Aruco_captures'):
         os.mkdir('./Aruco_captures')
@@ -13,6 +15,16 @@ def main():
     board = aruco.CharucoBoard_create(7, 5, 1, .8, aruco_dict)
     imboard = board.draw((2000, 2000))
     cv.imwrite("./chessboard.tiff", imboard)
+    stereoCam = CM.StereoCamera(IMGSIZE)
+    allCorners = []
+    allIds = []
+    decimator = 0
+    frame = stereoCam.GetFrame()
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    corners, ids, rejectedImgPoints = cv.aruco.detectMarkers(gray, aruco_dict)
+    if(len(corners) > 0):
+        # TODO : Save Points.
+        pass
 
 if __name__ == "__main__":
     main()
