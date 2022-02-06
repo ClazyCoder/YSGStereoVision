d6032f65e416c3c4fd4cd69f90e6f7fb2e0739bc
CLazyCoder <xots899@naver.com>
2022-02-06 19:22:34 +0900

    main.py point cloud 저장 기능완성, 파일 저장시 파일명 통일
    main.py의 유효 disparity 영역만 구하여 point cloud 저장 기능 추가.
    파일명 "00000000(년월일)-00시00분00초"로 저장되게끔 수정

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 import numpy as np
+import datetime
 import StereoModule.Calibration as SC
 import StereoModule.StereoMatcher as SSM
 import CameraModule.CameraModule as CM
@@ -19,10 +20,13 @@ end_header
 '''
 
 def main():
+    NUM_DISPARITY = 112
+    BLOCKSIZE = 15
+
     stereoCam = CM.StereoCamera(IMGSIZE)
     calibrator = SC.Calibrator(IMGSIZE)
     calibrator.LoadCalibrationDatas()
-    stereoMatcher = SSM.StereoMatcher(SSM.STEREO_TYPE_BM,112,15)
+    stereoMatcher = SSM.StereoMatcher(SSM.STEREO_TYPE_BM,NUM_DISPARITY,BLOCKSIZE)
     # stereoMatcher.leftMatcher.set...
     stereoMatcher.CreateWlsFilter()
     stereoMatcher.SetWlsFilterParameters(8000,1.5)
@@ -31,7 +35,7 @@ def main():
     R1, R2, P1, P2, Q, roi_left, roi_right = cv.stereoRectify(K1, D1, K2, D2, IMGSIZE, R, T ,alpha=0,flags=cv.CALIB_ZERO_DISPARITY)
     leftMapX, leftMapY = cv.initUndistortRectifyMap(K1, D1, R1, P1, IMGSIZE, cv.CV_32FC1)
     rightMapX, rightMapY = cv.initUndistortRectifyMap(K2, D2, R2, P2, IMGSIZE, cv.CV_32FC1)
-
+    valid_x1, valid_y1, valid_x2, valid_y2 = cv.getValidDisparityROI(roi_left,roi_right,numberOfDisparities=NUM_DISPARITY,blockSize=BLOCKSIZE)
     while True:
         leftImg, rightImg = stereoCam.GetFrame()
         
@@ -44,28 +48,45 @@ def main():
         disparity = stereoMatcher.GetDisparity(gray_left, gray_right)
         filteredDisp = stereoMatcher.GetFilteredDisparity(gray_left,gray_right)
         filteredDisp = filteredDisp.astype(np.float32)
-        image3D = cv.reprojectImageTo3D(filteredDisp, Q)
+        left_valid_rectified = left_rectified[valid_y1:valid_y1+valid_y2, valid_x1:valid_x1+valid_x2]
+        right_valid_rectified = right_rectified[valid_y1:valid_y1+valid_y2, valid_x1:valid_x1+valid_x2]
+        disparity_valid = filteredDisp[valid_y1:valid_y1+valid_y2, valid_x1:valid_x1+valid_x2]
+
+        image3D = cv.reprojectImageTo3D(disparity_valid, Q)
         depth = image3D[:,:,2].astype(np.float32)
 
-        mask = disparity > disparity.min()
+
+        mask = disparity_valid > disparity_valid.min()
 
         cv.normalize(depth,depth,0.0,1.0,cv.NORM_MINMAX)
         cv.normalize(disparity,disparity,1.0,0,cv.NORM_MINMAX)
         cv.normalize(filteredDisp,filteredDisp,1.0,0,cv.NORM_MINMAX)
         cv.imshow('left',leftImg)
         cv.imshow('right',rightImg)
-        cv.imshow('rectified_left',left_rectified)
-        cv.imshow('rectified_right',right_rectified)
+        #cv.imshow('rectified_left',left_rectified)
+        #cv.imshow('rectified_right',right_rectified)
+        cv.imshow('rectified_left_valid',left_valid_rectified)
+        cv.imshow('rectified_right_valid',right_valid_rectified)
         cv.imshow('disparity',disparity)
         cv.imshow('filteredDisparity',filteredDisp)
 
         k = cv.waitKey(1)
+
         if k == 27:
             break
+
         elif k == 99: # 'c'
-            out_points = points_3D[mask]
-            out_colors = colors[mask]
-            pass
+            today = datetime.datetime.today()
+            filename = './depth'+ str(today.year)+str(today.month)+str(today.day)+"-"+str(today.hour)+"시"+str(today.minute)+"분"+str(today.second)+"초"+".ply"
+            depth_points = depth[mask]
+            point_colors = left_valid_rectified[mask]
+
+            verts = depth_points.reshape(-1, 3)
+            point_colors = point_colors.reshape(-1, 3)
+            verts = np.hstack([verts, point_colors])
+            with open(filename, 'w') as f:
+                f.write(ply_header % dict(vert_num=len(verts)))
+                np.savetxt(f, verts, '%f %f %f %d %d %d')
     
 
 if __name__ == "__main__":
