56142b8b2487e05e92cec26e8923074919d6f559
CLazyCoder <xots899@naver.com>
2022-02-14 01:57:57 +0900

    cameramodule.py의 변수명 PEP8 스타일로 변경

--- cameramodule/cameramodule.py
+++ cameramodule/cameramodule.py
@@ -1,33 +1,33 @@
 import cv2 as cv
 
 class StereoCamera:
-    def __init__(self,imgSize):
-        self.leftCamera = cv.VideoCapture(0, cv.CAP_DSHOW)
-        self.leftCamera.set(cv.CAP_PROP_AUTOFOCUS, 0)
-        self.leftCamera.set(cv.CAP_PROP_AUTO_EXPOSURE,0)
-        self.leftCamera.set(cv.CAP_PROP_AUTO_WB,0)
-        self.leftCamera.set(cv.CAP_PROP_FRAME_WIDTH, imgSize[0])
-        self.leftCamera.set(cv.CAP_PROP_FRAME_HEIGHT, imgSize[1])
+    def __init__(self,img_size):
+        self.left_camera = cv.VideoCapture(0, cv.CAP_DSHOW)
+        self.left_camera.set(cv.CAP_PROP_AUTOFOCUS, 0)
+        self.left_camera.set(cv.CAP_PROP_AUTO_EXPOSURE,0)
+        self.left_camera.set(cv.CAP_PROP_AUTO_WB,0)
+        self.left_camera.set(cv.CAP_PROP_FRAME_WIDTH, img_size[0])
+        self.left_camera.set(cv.CAP_PROP_FRAME_HEIGHT, img_size[1])
 
-        self.rightCamera = cv.VideoCapture(1, cv.CAP_DSHOW)
-        self.rightCamera.set(cv.CAP_PROP_AUTOFOCUS, 0)
-        self.rightCamera.set(cv.CAP_PROP_AUTO_EXPOSURE,0)
-        self.rightCamera.set(cv.CAP_PROP_AUTO_WB,0)
-        self.rightCamera.set(cv.CAP_PROP_FRAME_WIDTH, imgSize[0])
-        self.rightCamera.set(cv.CAP_PROP_FRAME_HEIGHT, imgSize[1])
+        self.right_camera = cv.VideoCapture(1, cv.CAP_DSHOW)
+        self.right_camera.set(cv.CAP_PROP_AUTOFOCUS, 0)
+        self.right_camera.set(cv.CAP_PROP_AUTO_EXPOSURE,0)
+        self.right_camera.set(cv.CAP_PROP_AUTO_WB,0)
+        self.right_camera.set(cv.CAP_PROP_FRAME_WIDTH, img_size[0])
+        self.right_camera.set(cv.CAP_PROP_FRAME_HEIGHT, img_size[1])
         
     def get_frame(self):
-        if not (self.leftCamera.grab()and self.rightCamera.grab()):
+        if not (self.left_camera.grab()and self.right_camera.grab()):
             print("No more frames")
             return None
-        ret_left, frame_left = self.leftCamera.retrieve()
-        ret_right, frame_right = self.rightCamera.retrieve()
+        ret_left, frame_left = self.left_camera.retrieve()
+        ret_right, frame_right = self.right_camera.retrieve()
         return (frame_left, frame_right)
         
     def get_frame_with_ret(self):
-        if not (self.leftCamera.grab()and self.rightCamera.grab()):
+        if not (self.left_camera.grab()and self.right_camera.grab()):
             print("No more frames")
             return None
-        ret_left, frame_left = self.leftCamera.retrieve()
-        ret_right, frame_right = self.rightCamera.retrieve()
+        ret_left, frame_left = self.left_camera.retrieve()
+        ret_right, frame_right = self.right_camera.retrieve()
         return (ret_left, frame_left, ret_right, frame_right)
