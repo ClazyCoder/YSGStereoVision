1b59bd6be4983c8f19cbca2bde54c40ad1c13a4d
CLazyCoder <xots899@naver.com>
2022-02-21 03:10:46 +0900

    cameramodule.py 주석작업.

--- cameramodule/cameramodule.py
+++ cameramodule/cameramodule.py
@@ -2,6 +2,9 @@ import cv2 as cv
 
 class StereoCamera:
     def __init__(self,img_size):
+        '''
+        스테레오 카메라 클래스.
+        '''
         self.left_camera = cv.VideoCapture(0, cv.CAP_DSHOW)
         self.left_camera.set(cv.CAP_PROP_AUTOFOCUS, 0)
         self.left_camera.set(cv.CAP_PROP_AUTO_EXPOSURE,0)
@@ -17,6 +20,9 @@ class StereoCamera:
         self.right_camera.set(cv.CAP_PROP_FRAME_HEIGHT, img_size[1])
         
     def get_frame(self):
+        '''
+        두 카메라로 부터 얻은 영상을 반환하는 메서드
+        '''
         if not (self.left_camera.grab()and self.right_camera.grab()):
             print("No more frames")
             return None
@@ -25,6 +31,9 @@ class StereoCamera:
         return (frame_left, frame_right)
         
     def get_frame_with_ret(self):
+        '''
+        get_frame 메서드에서 retrieve 값을 추가적으로 반환하는 메서드
+        '''
         if not (self.left_camera.grab()and self.right_camera.grab()):
             print("No more frames")
             return None
