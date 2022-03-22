a00241ce27d2865ec9c69bbab7691aef68e9a4f7
CLazyCoder <xots899@naver.com>
2022-03-23 03:57:55 +0900

    stereomatcher.py StereoMatcher 클래스 메서드의 패러미터 관련 코멘트 추가.

--- stereomodule/stereomatcher.py
+++ stereomodule/stereomatcher.py
@@ -123,59 +123,121 @@ class StereoMatcher:
 
     def set_uniqueness_ratio(self, uniqueness_ratio):
         '''
+        # set_uniqueness_ratio
         BM과 SGBM 클래스의 UniquenessRatio 패러미터를 설정하는 메서드
+
+        ## Parameters
+        - uniqueness_ratio
+
+        ## uniqueness_ratio
+        매칭시 비용함수에서 가장 높은 점수를 얻는지점(가장 확실하다고 보이는 disparity)이 없으면,\n
+        그때 1등과 2등간의 임계점을 결정해주는 패러미터이다.
         '''
         self.left_matcher.setUniquenessRatio(uniqueness_ratio)
 
     def set_texture_threshold(self, texture_threshold):
         '''
+        # set_texture_threshold
         BM 클래스의 TextureThreshold 패러미터를 설정하는 메서드
+
+        ## Parameters
+        - texture_threshold
+
+        ### texture_threshold
+        블록 매칭시 이 임계값 보다 낮은 텍스처 값(이미지 미분의 절대합)을\n
+        지니는 지점은 disparity가 결정되지 않는다.
         '''
         assert self.stereo_type == STEREO_TYPE_BM, 'Only BM has TextureThreshold parameter!'
         self.left_matcher.setTextureThreshold(texture_threshold)
     
-    def set_smaller_block_size(self, smaller_block_size):
-        '''
-        BM 클래스의 SmallerBlockSize 패러미터를 설정하는 메서드
-        '''
-        assert self.stereo_type == STEREO_TYPE_BM, 'Only BM has SmallerBlockSize parameter!'
-        self.left_matcher.setSmallerBlockSize(smaller_block_size)
-
     def set_speckle_range(self, speckle_range):
         '''
+        # set_speckle_range
         BM과 SGBM 클래스의 SpeckleRange 패러미터를 설정하는 메서드
+
+        ## Parameters
+        - speckle_range
+
+        ### speckle_range
+        블록 매칭시 주변 Disparity들에 비해 튀는 값을 가지는 작은 블롭(Blob)들을 제거하는 후처리 필터링 과정에 사용되는 패러미터이다.\n
+        Blob과 그 주변 픽셀이 같은 덩어리인지를 결정할때 기준이 되는 범위 값이다. 
         '''
         self.left_matcher.setSpeckleRange(speckle_range)
 
     def set_speckle_window_size(self, speckle_window_size):
         '''
+        # set_peckle_window_size
         BM과 SGBM 클래스의 SpeckleWindowSize 패러미터를 설정하는 메서드
+
+        ## Parameters
+        - speckle_window_size
+
+        ### speckle_window_size
+        블록 매칭시 주변 Disparity들에 비해 튀는 값을 가지는 작은 블롭(Blob)들을 제거하는 후처리 필터링 과정에 사용되는 패러미터이다.\n
+        Blob의 크기를 결정하는 패러미터 이다.
         '''
         self.left_matcher.setSpeckleWindowSize(speckle_window_size)
 
-    def set_disp23_max_diff(self, disp23_max_diff):
-        '''
-        BM과 SGBM 클래스의 Disp12MaxDiff 패러미터를 설정하는 메서드
-        '''
-        self.left_matcher.setDisp12MaxDiff(disp23_max_diff)
-
     def set_p1(self, p1):
         '''
+        # set_p1
         SGBM 클래스의 P1 패러미터를 설정하는 메서드
+
+        ## Parameters
+        - p1
+
+        ### p1
+        SGM(Semi-Global Matching)에 사용되는 패러미터이다.\n
+        주변 픽셀의 수가 하나일때 disparity 차이에 대한 패널티 값이다.
         '''
         assert self.stereo_type == STEREO_TYPE_SGBM, 'Only SGBM has P1 parameter!'
         self.left_matcher.setP1(p1)
 
     def set_p2(self, p2):
         '''
+        # set_p2
         SGBM 클래스의 P2 패러미터를 설정하는 메서드
+
+        ## Parameters
+        - p2
+
+        ### p2
+        SGM(Semi-Global Matching)에 사용되는 패러미터이다.\n
+        주변 픽셀의 수가 둘 이상일때 disparity 차이에 대한 패널티 값이다.
         '''
         assert self.stereo_type == STEREO_TYPE_SGBM, 'Only SGBM has P2 parameter!'
         self.left_matcher.setP2(p2)
 
+    def set_disp12_max_diff(self, disp12_max_diff):
+        '''
+        # set_disp12_max_diff
+        SGBM 클래스의 Disp12MaxDiff 패러미터를 설정하는 메서드
+
+        ## Parameters
+        - disp12_max_diff
+
+        ### disp12_max_diff
+        왼쪽에서 오른쪽으로의 disparity 계산결과와 오른쪽에서 왼쪽으로의 계산결과를 비교하여,\n 
+        이 값보다 차이가 클 경우, 그 픽셀의 disparity는 결정되지 않도록하는 패러미터이다.
+        '''
+        assert self.stereo_type == STEREO_TYPE_SGBM, 'Only SGBM has Disp12MaxDiff parameter!'
+        self.left_matcher.setDisp12MaxDiff(disp12_max_diff)
+
     def set_mode(self, mode):
         '''
+        # set_mode
         SGBM 클래스의 Mode 패러미터를 설정하는 메서드
+
+        ## Parameters
+        - mode
+
+        ### mode
+        SGBM의 계산 방식을 결정하는 패러미터이다.\n
+        모드의 종류는 다음과 같다.
+        - cv2.STEREO_SGBM_MODE_SGBM (기본값)
+        - cv2.STEREO_SGBM_MODE_SGBM_3WAY
+        - cv2.STEREO_SGBM_MODE_HH
+        - cv2.STEREO_SGBM_MODE_HH4
         '''
         assert self.stereo_type == STEREO_TYPE_SGBM, 'Only SGBM has Matching Mode Parameter!'
         self.left_matcher.setMode(mode)
